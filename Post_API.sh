d0ac650d642006c250d7a599e5f13c5f7e1f7502
Prajwal J <118866033+prajwal1691@users.noreply.github.com>
2023-02-09 17:11:10 +0530

    Create Post_API.sh

---
+++ Post_API.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+kubeadm init
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+export KUBECONFIG=$HOME/.kube/config
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
