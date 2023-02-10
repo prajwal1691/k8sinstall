60ac096ebd2046878383ff7b50a70905846026aa
Prajwal J <118866033+prajwal1691@users.noreply.github.com>
2023-02-10 12:44:24 +0530

    Update Post_API.sh

--- Post_API.sh
+++ Post_API.sh
@@ -4,4 +4,4 @@ mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 export KUBECONFIG=$HOME/.kube/config
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
